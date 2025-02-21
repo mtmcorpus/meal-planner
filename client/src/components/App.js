bb5fe210f73ec83a1c575535bb922cdbbdbd6c4d
mtmcorpus <mtmcorpus@gmail.com>
2025-02-21 20:10:02 +0800

    Updated scripts to run client and server

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Meal Planner App</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <div id="root"></div>
-    <script src="../src/index.js"></script>
-</body>
-</html>
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to the Meal Planner App!</h1>
+    </div>
+  );
+};
+
+export default App;
